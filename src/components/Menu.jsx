7c79c90f6da459cb561f16c14130bb1ab2795afe
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-01-16 11:47:54 -0300

    criando componente Menu para página inicial

---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+export default function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">Serviços</a>
+        </li>
+        <li>
+          <a href="">Equipe</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
